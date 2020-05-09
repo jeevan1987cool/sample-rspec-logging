fd4131c4e26d93c17eede2ce906d337cb49fe5bd
Jeevan <jeev1987cool@gmail.com>
2020-05-09 14:14:33 +0200

    update tests with new logger

--- spec/first_test.rb
+++ spec/first_test.rb
@@ -1,13 +1,15 @@
 require_relative 'spec_helper'
+require_relative '../helper/logger_util'
+
 
 describe 'test_feature', tms_1: 'QA-123', tms_2: 'QA-124' do
   it 'test_case_1' do
     expect(20).to be > 19
-    logger.info 'Info Passed ********************'
+    custom_logger.logger.info 'Info Passed ********************'
   end
 
   it 'test_case_2' do
     expect(21).to be < 19
-    logger.error 'Info Failed ********************'
+    custom_logger.logger.error 'Info Failed ********************'
   end
 end
